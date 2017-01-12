7cea4eda44f7de20406d664c98f7fdaf770dfd0a
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-01-12 10:34:35 +0545

    env var SITE_URL set

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,13 +1,11 @@
 ActionMailer::Base.register_interceptor(SendGrid::MailInterceptor)
 
-if Rails.env.production? || Rails.env.development?
-	ActionMailer::Base.smtp_settings = {
-	     :user_name => ENV["NT_SEND_GRID_USER_NAME"],
-	     :password => ENV["NT_SEND_GRID_PASSWORD"],
-	     :domain => 'http://www.salesmatchr.com/',
-	     :address => 'smtp.sendgrid.net',
-	     :port => 587,
-	     :authentication => :plain,
-	     :enable_starttls_auto => true
-	 }
-end
+ActionMailer::Base.smtp_settings = {
+     :user_name => ENV["NT_SEND_GRID_USER_NAME"],
+     :password => ENV["NT_SEND_GRID_PASSWORD"],
+     :domain => ENV["SITE_URL"] || 'http://www.salesmatchr.com/',
+     :address => 'smtp.sendgrid.net',
+     :port => 587,
+     :authentication => :plain,
+     :enable_starttls_auto => true
+ }
