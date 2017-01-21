b9b6fcd726d58fb654b1b70c3d1a84c77ce02572
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-01-21 15:22:34 +0545

    Association of answer and question model corrected

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
-  has_many :answers
+  has_many :candidate_question_answers
+  has_many :answers, through: :candidate_question_answers
   accepts_nested_attributes_for :answers, allow_destroy: true
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Answer do
 	describe "Association" do
-	  it {should have_many :questions}
     it {should have_many :candidate_question_answers}
+    it {should have_many(:questions).through(:candidate_question_answers) }
 	end
 end
