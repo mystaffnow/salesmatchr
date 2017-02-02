d588f8f01b92d1f9843547e7a612d24f899ed3e2
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-02-02 18:44:14 +0545

    Employer model fixed after normalization

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -35,11 +35,8 @@ class Employer < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  belongs_to :state
+  has_one :employer_profile, dependent: :destroy
   has_many :jobs
-
-  has_attached_file :avatar,  :default_url => "/img/missing.png", :styles => { :medium => "200x200#" }
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   def name
     "#{self.first_name} #{self.last_name}"
@@ -50,6 +47,7 @@ class Employer < ActiveRecord::Base
   end
 
   def can_proceed
-    self.state.present? && self.city.present? && self.zip.present? && self.website.present? && self.name.present? && self.email.present?
+    return false if self.employer_profile.nil?
+    self.employer_profile.state.present? && self.employer_profile.city.present? && self.employer_profile.zip.present? && self.employer_profile.website.present? && self.name.present? && self.email.present?
   end
 end
