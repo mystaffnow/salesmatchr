a2b510c73f2d66e2d80b7357c31038f989c89d8f
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-02-10 17:38:00 +0545

    policy class candidate_job_action created and added policy definition for action candidate_save_job

---
+++ app/policies/candidate_job_action_policy.rb
@@ -0,0 +1,13 @@
+class CandidateJobActionPolicy < ApplicationPolicy
+	attr_reader :user, :candidate_job_action
+
+	def initialize(user, candidate_job_action)
+		@user = user
+		@candidate_job_action = candidate_job_action
+	end
+
+	def candidate_save_job?
+		binding.pry
+		user.is_owner_of?(candidate_job_action)
+	end
+end
