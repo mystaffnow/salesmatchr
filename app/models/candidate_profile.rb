129cee68f40fb64cbc79e9e05bc58c337cf2dbb6
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-02-13 19:14:05 +0545

    loaded updated schema info

--- app/models/candidate_profile.rb
+++ app/models/candidate_profile.rb
@@ -1,9 +1,29 @@
+# == Schema Information
+#
+# Table name: candidate_profiles
+#
+#  id                   :integer          not null, primary key
+#  candidate_id         :integer
+#  city                 :string
+#  state_id             :integer
+#  zip                  :string
+#  education_level_id   :integer
+#  ziggeo_token         :string
+#  is_incognito         :boolean
+#  linkedin_picture_url :string
+#  avatar_file_name     :string
+#  avatar_content_type  :string
+#  avatar_file_size     :integer
+#  avatar_updated_at    :datetime
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
+
 class CandidateProfile < ActiveRecord::Base
 	belongs_to :candidate
 	belongs_to :state
 	belongs_to :education_level
 	
-  # attr_accessor :avatar
   has_attached_file :avatar,  :default_url => "/img/missing.png", :styles => { :medium => "200x200#" }
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
--- app/models/employer_profile.rb
+++ app/models/employer_profile.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: employer_profiles
+#
+#  id                  :integer          not null, primary key
+#  employer_id         :integer
+#  website             :string
+#  ziggeo_token        :string
+#  zip                 :string
+#  city                :string
+#  state_id            :integer
+#  description         :string
+#  avatar_file_name    :string
+#  avatar_content_type :string
+#  avatar_file_size    :integer
+#  avatar_updated_at   :datetime
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+
 class EmployerProfile < ActiveRecord::Base
 	belongs_to :employer
 	belongs_to :state
