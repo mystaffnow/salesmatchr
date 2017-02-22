16faee4f3c7c4cb6c91ad46e6e30bc14325a58d4
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-02-22 14:32:49 +0545

    fn, ln, company_name of employer are required

--- spec/factories/employer.rb
+++ spec/factories/employer.rb
@@ -2,6 +2,9 @@ FactoryGirl.define do
   factory :employer do
     sequence(:email) {|i| "test#{i}@example.com"}
     password 'password'
+    first_name 'emp first name'
+    last_name 'emp last name'
+    company 'Emp company name'
   end
 
   factory :archetype_employer, class: Employer do
