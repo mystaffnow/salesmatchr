26377020ebac1079fd3173174a22eb7ed15889ee
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-02-24 18:41:20 +0545

    some model specs failed during changes on model code which are passed now

--- spec/models/candidate_profile_spec.rb
+++ spec/models/candidate_profile_spec.rb
@@ -25,11 +25,6 @@ RSpec.describe CandidateProfile, type: :model do
 	let(:state) {create(:state)}
 	let(:education_level) {create(:education_level)}
 	let(:candidate) {create(:candidate, :first_name => "test", :last_name => "candidate")}
-	let(:candidate_profile) {create(:candidate_profile,
-																 :candidate_id => candidate.id,
-																 :state_id => state.id,
-																 :education_level_id => education_level.id
-																 )}
 
 	describe '.validation' do
 		it { should validate_uniqueness_of(:candidate_id)}
@@ -45,13 +40,11 @@ RSpec.describe CandidateProfile, type: :model do
 		it { should have_attached_file(:avatar) }
 	  it { should validate_attachment_content_type(:avatar).
 	  	allowing('image/jpg', 'image/png', 'image/gif').
-	  	rejecting('text/plain', 'text/xml') }
-	  
-	  context '.shoulda matcher' do
-	  	it 'should return default avatar' do
-	  		expect(candidate_profile.avatar_url).to eq('/img/missing.png')
-	  	end
-	  end
+	  	rejecting('text/plain', 'text/xml')
+	  	 }
+  	it 'should return default avatar' do
+  		@candidate1 = create(:candidate, :first_name => "test", :last_name => "candidate")
+  		expect(CandidateProfile.first.avatar_url).to eq('/img/missing.png')
+  	end
 	end
 end
-	
