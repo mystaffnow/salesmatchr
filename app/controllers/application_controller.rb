42c14b0f35a7810c7792c93f2dacbf6ce3575c0a
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-03-08 19:44:05 +0545

    token skip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   include Pundit
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+  skip_before_filter :verify_authenticity_token
   before_filter :check_candidate
   before_filter :check_employer
   before_action :configure_permitted_parameters, if: :devise_controller?
