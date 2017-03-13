59b4ded29fd02126ded6575fb4337c346f45f770
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-03-13 16:19:56 +0545

    spec checked for incognito ON/OFF

--- spec/models/candidate_profile_spec.rb
+++ spec/models/candidate_profile_spec.rb
@@ -43,9 +43,32 @@ RSpec.describe CandidateProfile, type: :model do
 	  	allowing('image/jpg', 'image/png', 'image/gif').
 	  	rejecting('text/plain', 'text/xml')
 	  	 }
-  	it 'should return default avatar' do
-  		@candidate1 = create(:candidate, :first_name => "test", :last_name => "candidate")
+
+  	it 'should return default incognito image when incognito ON, avatar nil' do
+  		candidate
+  		expect(CandidateProfile.first.avatar_url).to eq('/img/incognito.png')
+  	end
+
+  	it 'should return missing image when incognito OFF, avatar nil' do
+  		candidate
+  		CandidateProfile.first.update(is_incognito: false)
+  		expect(CandidateProfile.count).to eq(1)
+  		expect(CandidateProfile.first.is_incognito).to be_falsy
   		expect(CandidateProfile.first.avatar_url).to eq('/img/missing.png')
   	end
+
+  	it 'should return image when incognito OFF, avatar present' do
+  		candidate
+  		file = File.open("public/img/Graphic2.png")
+  		profile = CandidateProfile.first
+  		profile.update_attributes(is_incognito: false, avatar: file)
+  		profile.save
+  		expect(CandidateProfile.first.avatar_file_name).to eq('Graphic2.png')
+  	end
+
+    it 'should have job matching email alert setting ON by default' do
+      candidate
+      expect(CandidateProfile.first.is_active_match_subscription).to be_truthy
+    end
 	end
 end
