639b047c229ba7a8112159e00a38b3035c1efc56
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-03-13 16:20:47 +0545

    validations, association spec

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -12,8 +12,14 @@
 require 'rails_helper'
 
 RSpec.describe Answer do
-	describe "Association" do
+	describe ".Association" do
     it {should have_many :candidate_question_answers}
     it {should have_many(:questions).through(:candidate_question_answers) }
+	end
+
+	describe '.validation' do
+		it {should validate_presence_of :name}
+		it {should validate_uniqueness_of :name}
+		it {should validate_presence_of :score}
 	end
 end
