66590a833a53e619fc3831a45f3c4046900ee874
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-03-23 15:24:30 +0545

    Candidate should not see disable jobs in his job_viewed list

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -137,6 +137,18 @@ RSpec.describe Job do
                                                            is_saved: false)
       expect(Job.job_saved_list(@candidate)).to eq([@job])
     end
+
+    it 'should return nil when job is inactive and disabled' do
+      @inside_sales = create(:inside_sales)
+      @candidate = create(:candidate, archetype_score: 35)
+      @job1 = create(:job, job_function_id: @inside_sales.id, is_active: false, status: Job.statuses["disable"])
+      state = create(:state, name: 'Title 1')
+      @job = create(:job, state_id: state.id, job_function_id: @inside_sales.id)
+      expect(Job.job_matched_list(@candidate)).to eq([@job])
+      expect(Job.first.is_active).to be_falsy
+      expect(Job.first.status).to eq('disable')
+      expect(Job.job_saved_list(@candidate)).not_to eq([@job1])
+    end
   end
 
   context '#visible_candidate_viewed_list' do
