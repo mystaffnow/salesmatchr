7e3e29fd590078e2208a8f28df155500616e35a1
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-03-30 13:41:32 +0545

    Added pagination

--- app/controllers/candidates/sessions_controller.rb
+++ app/controllers/candidates/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Candidates::SessionsController < Devise::SessionsController
   skip_before_filter :check_candidate, only: [:destroy]
   def after_sign_in_path_for(resource)
-    '/job_candidates'
+    '/my_jobs'
   end
 end
