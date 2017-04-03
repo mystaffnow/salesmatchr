26d0bfbeeb938f940f07268b768368584783e60b
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-04-03 15:32:30 +0545

    sendgrid template configure at sendgrid and calling on our application

--- app/mailers/employer_mailer.rb
+++ app/mailers/employer_mailer.rb
@@ -3,6 +3,7 @@ class EmployerMailer < ApplicationMailer
   
   def send_job_application(email, job)
     @job = job
+    template_id(ENV['SENDGRID_TEMPLATE_ID'])
     mail( :to => email,
           :subject => 'Someone has applied to your job at SalesMatchr!' ) do |format|
       format.html { render layout: false }
@@ -10,6 +11,7 @@ class EmployerMailer < ApplicationMailer
   end
   def send_job_withdrawn(email, job)
     @job = job
+    template_id(ENV['SENDGRID_TEMPLATE_ID'])
     mail( :to => email,
           :subject => 'Someone has withdrawn an application for your job at SalesMatchr' ) do |format|
       format.html { render layout: false }
