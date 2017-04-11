b068b31ff2653d70d2f0c30ca13ee09a333a7c57
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-04-11 18:03:17 +0545

    added comment

--- spec/support/stripe.rb
+++ spec/support/stripe.rb
@@ -1,3 +1,4 @@
+# return stripe card token
 def generate_stripe_card_token
 	Stripe.api_key = "sk_test_4Bqn5DAx7IfmDeECYppLl1PY"
 	result = Stripe::Token.create(
@@ -8,10 +9,10 @@ def generate_stripe_card_token
 	    :cvc => "314"
 	  },
 	)
-
 	result.id
 end
 
+# return stripe customer id
 def generate_stripe_customer(stripe_card_token)
 	Stripe.api_key = "sk_test_4Bqn5DAx7IfmDeECYppLl1PY"
 	result = Stripe::Customer.create(card: stripe_card_token)
