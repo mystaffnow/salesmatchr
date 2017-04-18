80314c055063db6aa55d84511c265387529b88f3
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-04-18 14:54:45 +0545

    show feature added on payment, disable unwanted column

--- app/admin/payment.rb
+++ app/admin/payment.rb
@@ -25,11 +25,22 @@ ActiveAdmin.register Payment do
   		pay.job.present? ? pay.job.title : ''
   	end
   	column :amount
-  	column :stripe_card_token
+  	# column :stripe_card_token
   	column :stripe_customer_id
   	column :stripe_charge_id
   	column :status
 
   	actions
+  end
+
+  show do |payment|
+    attributes_table do
+      row :employer_id
+      row :job_id
+      row :amount
+      row :stripe_customer_id
+      row :stripe_charge_id
+      row :status
+    end
   end
 end
