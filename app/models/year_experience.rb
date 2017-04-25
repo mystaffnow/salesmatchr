342950d05877b3d93bb200748f602d0b03b053d1
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-04-25 16:00:43 +0545

    model refactoring

--- app/models/year_experience.rb
+++ app/models/year_experience.rb
@@ -13,6 +13,6 @@
 class YearExperience < ActiveRecord::Base
 	# validation
 	validates_presence_of :name, :low, :high
-	validates_uniqueness_of :name 
+	validates_uniqueness_of :name
 	# validation
 end
