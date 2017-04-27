352747fab27580b2f440d6a61363ceb09830745a
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-04-27 14:20:50 +0545

    factroy spec for answer added

--- spec/factories/answer.rb
+++ spec/factories/answer.rb
@@ -3,4 +3,24 @@ FactoryGirl.define do
     name "Strongly Agree"
     score 5
   end
+
+  factory :agree, class: Answer do
+    name "Agree"
+    score 5
+  end
+
+  factory :neutral, class: Answer do
+    name "neutral"
+    score 5
+  end
+
+  factory :strongly_disagree, class: Answer do
+    name "Strongly Disagree"
+    score 5
+  end
+
+  factory :disagree, class: Answer do
+    name "Disagree"
+    score 5
+  end
 end
