5998fd2ee85cf642473c9af77e4a09ad91eacfac
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-04-27 14:20:30 +0545

    rspec added to test calculate_archetype_score

--- spec/models/candidate_question_answer_spec.rb
+++ spec/models/candidate_question_answer_spec.rb
@@ -18,4 +18,59 @@ RSpec.describe CandidateQuestionAnswer do
     it {should belong_to :question}
     it {should belong_to :answer}
   end
+
+  it '#calculate_archetype_score' do
+    candidate = create(:candidate)
+    
+    question = create(:question)
+    question2 = create(:question_second)
+    question3 = create(:question_third)
+    question4 = create(:question_fourth)
+    question5 = create(:question_fifth)
+    question6 = create(:question_sixth)
+    question7 = create(:question_seventh)
+    question8 = create(:question_eighth)
+    question9 = create(:question_ninth)
+    question10 = create(:question_tenth)
+    question11 = create(:question_eleventh)
+    question12 = create(:question_twelveth)
+    question13 = create(:question_thirteenth)
+    question14 = create(:question_fourteenth)
+    question15 = create(:question_fifteenth)
+    question16 = create(:question_sixteenth)
+    question17 = create(:question_seventeenth)
+    question18 = create(:question_eighteenth)
+    question19 = create(:question_nineteenth)
+    question20 = create(:question_twentieth)
+    
+    strongly_agree = create(:answer)
+    agree = create(:agree)
+    strongly_disagree = create(:strongly_disagree)
+    neutral = create(:neutral)
+    disagree = create(:disagree)
+    
+    create(:candidate_question_answer, candidate_id: candidate.id, question_id: question.id, answer_id: strongly_agree.id)
+    create(:candidate_question_answer, candidate_id: candidate.id, question_id: question2.id, answer_id: agree.id)
+    create(:candidate_question_answer, candidate_id: candidate.id, question_id: question3.id, answer_id: strongly_disagree.id)
+    create(:candidate_question_answer, candidate_id: candidate.id, question_id: question4.id, answer_id: neutral.id)
+    create(:candidate_question_answer, candidate_id: candidate.id, question_id: question5.id, answer_id: disagree.id)
+    create(:candidate_question_answer, candidate_id: candidate.id, question_id: question6.id, answer_id: strongly_agree.id)
+    create(:candidate_question_answer, candidate_id: candidate.id, question_id: question7.id, answer_id: agree.id)
+    create(:candidate_question_answer, candidate_id: candidate.id, question_id: question8.id, answer_id: strongly_disagree.id)
+    create(:candidate_question_answer, candidate_id: candidate.id, question_id: question9.id, answer_id: neutral.id)
+    create(:candidate_question_answer, candidate_id: candidate.id, question_id: question10.id, answer_id: disagree.id)
+    create(:candidate_question_answer, candidate_id: candidate.id, question_id: question11.id, answer_id: strongly_agree.id)
+    create(:candidate_question_answer, candidate_id: candidate.id, question_id: question12.id, answer_id: agree.id)
+    create(:candidate_question_answer, candidate_id: candidate.id, question_id: question13.id, answer_id: strongly_disagree.id)
+    create(:candidate_question_answer, candidate_id: candidate.id, question_id: question14.id, answer_id: neutral.id)
+    create(:candidate_question_answer, candidate_id: candidate.id, question_id: question15.id, answer_id: disagree.id)
+    create(:candidate_question_answer, candidate_id: candidate.id, question_id: question16.id, answer_id: strongly_agree.id)
+    create(:candidate_question_answer, candidate_id: candidate.id, question_id: question17.id, answer_id: agree.id)
+    create(:candidate_question_answer, candidate_id: candidate.id, question_id: question18.id, answer_id: strongly_disagree.id)
+    create(:candidate_question_answer, candidate_id: candidate.id, question_id: question19.id, answer_id: neutral.id)
+    create(:candidate_question_answer, candidate_id: candidate.id, question_id: question20.id, answer_id: disagree.id)
+  
+    expect(CandidateQuestionAnswer.calculate_archetype_score(candidate)).not_to be_nil
+    expect(CandidateQuestionAnswer.calculate_archetype_score(candidate)).to eq(100)
+  end
 end
