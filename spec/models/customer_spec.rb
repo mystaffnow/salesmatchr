2353b8a8d7be8518d1e2284d229c51cc1253014f
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-05-02 14:52:44 +0545

    validation spec added

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -22,7 +22,8 @@ RSpec.describe Customer, type: :model do
     it {should validate_presence_of :stripe_card_token}
     it {should validate_presence_of :stripe_customer_id}
     it {should validate_presence_of :last4}
-    it {should validate_uniqueness_of :employer_id}
+    it {should validate_presence_of :card_number}
+    it {should validate_uniqueness_of :card_number}
   end
 
   describe 'association' do
