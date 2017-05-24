f550392a8c43a7ec8c23ea80016be2e80e5965dd
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-05-24 15:25:50 +0545

    spec put/account red to green refactoring

--- spec/controllers/employers_controller_spec.rb
+++ spec/controllers/employers_controller_spec.rb
@@ -22,7 +22,8 @@ RSpec.describe EmployersController, :type => :controller do
       }
 
       it 'should redirect to /employers/account' do
-        EmployerProfile.first.update(zip: nil, state_id: nil, city: nil, website: nil)
+        blank_profile(EmployerProfile.first)
+        # EmployerProfile.first.update(zip: nil, state_id: nil, city: nil, website: nil)
         get :profile
         expect(response).to redirect_to("/employers/account")
       end
